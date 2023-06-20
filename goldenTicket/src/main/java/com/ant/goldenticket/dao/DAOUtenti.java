f35f24964e82866834e0e0819f724942f95ce048
Andrea Sbabo <andreasbabo6@gmail.com>
2023-06-20 11:46:23 +0200

    Added read & update

--- goldenTicket/src/main/java/com/ant/goldenticket/dao/DAOUtenti.java
+++ goldenTicket/src/main/java/com/ant/goldenticket/dao/DAOUtenti.java
@@ -12,4 +12,13 @@ public class DAOUtenti {
 	public List<Map<String,String>> readAll(){
 		return db.rows("select * from utenti");
 	}
+	
+	public Map<String,String> readByID(int id){
+		return db.row("select * from utenti where id=?",id+"");
+	}
+	public boolean create(Map<String,String> u) {
+		String query="update utenti set username = ? , password = ? , ruolo = ? where id = ?";
+		
+		return db.update(query, u.get("username"),u.get("password"),u.get("ruolo"),u.get("id"));
+	}
 }
