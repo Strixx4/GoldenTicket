9caf28c07e0a92727beee5af6bf0277ed780af7f
michelepasino <michele.pasino03@gmail.com>
2023-06-20 12:54:03 +0200

    bean

--- goldenTicket/src/main/java/com/ant/goldenticket/Context.java
+++ goldenTicket/src/main/java/com/ant/goldenticket/Context.java
@@ -1,10 +1,14 @@
 package com.ant.goldenticket;
 
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.ant.goldenticket.dao.DAOLocalita;
 import com.ant.goldenticket.dao.Database;
+import com.ant.goldenticket.entities.Localita;
 
 @Configuration
 public class Context {
@@ -12,9 +16,19 @@ public class Context {
 	public Database db() {
 		return new Database("GoldenTicket","root","root");
 	}
+
 	@Bean 
 	DAOLocalita daolocalita()
 	{
 		return new DAOLocalita();
+	}
+	
+	@Bean
+	@Scope("prototype")
+	public Localita mappalocalita(Map<String,String> m)
+	{
+		Localita l = new Localita();
+		l.fromMap(m);
+		return l;
 	}
 }
