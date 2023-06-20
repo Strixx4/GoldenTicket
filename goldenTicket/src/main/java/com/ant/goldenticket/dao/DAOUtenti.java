56bb636a1eebc9e899072ea077497c648ca026d1
Andrea Sbabo <andreasbabo6@gmail.com>
2023-06-20 10:58:28 +0200

    Update DAOUtenti.java

--- goldenTicket/src/main/java/com/ant/goldenticket/dao/DAOUtenti.java
+++ goldenTicket/src/main/java/com/ant/goldenticket/dao/DAOUtenti.java
@@ -1,5 +1,15 @@
 package com.ant.goldenticket.dao;
 
-public class DAOUtenti {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DAOUtenti {
+	@Autowired
+	private Database db;
+	
+	public List<Map<String,String>> readAll(){
+		return db.rows("select * from utenti");
+	}
 }
