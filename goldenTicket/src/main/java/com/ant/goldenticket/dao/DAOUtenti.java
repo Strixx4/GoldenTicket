d9289707bb0fb73d24557692fec732eae0eb2e4c
Andrea Sbabo <andreasbabo6@gmail.com>
2023-06-20 12:08:22 +0200

    Update DAOUtenti.java

--- goldenTicket/src/main/java/com/ant/goldenticket/dao/DAOUtenti.java
+++ goldenTicket/src/main/java/com/ant/goldenticket/dao/DAOUtenti.java
@@ -16,9 +16,19 @@ public class DAOUtenti {
 	public Map<String,String> readByID(int id){
 		return db.row("select * from utenti where id=?",id+"");
 	}
-	public boolean create(Map<String,String> u) {
+	public boolean update(Map<String,String> u) {
 		String query="update utenti set username = ? , password = ? , ruolo = ? where id = ?";
 		
 		return db.update(query, u.get("username"),u.get("password"),u.get("ruolo"),u.get("id"));
+	}
+	
+	public boolean delete(int id) {
+		return db.update("delete from utenti where id = ?",id+"");
+	}
+	
+	//I controlli saranno messi in controller
+	public boolean create(Map<String,String> u) {
+		String query= "insert into utenti (username,password,ruolo) values (?,?,?)";
+		return db.update(query, u.get("username"),u.get("password"),u.get("ruolo"));
 	}
 }
