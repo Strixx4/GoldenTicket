229a23d00c2af36408fc2ddec2c158e7866e7f49
michelepasino <michele.pasino03@gmail.com>
2023-06-20 12:41:21 +0200

    update context with daolocalita

--- goldenTicket/src/main/java/com/ant/goldenticket/Context.java
+++ goldenTicket/src/main/java/com/ant/goldenticket/Context.java
@@ -3,6 +3,7 @@ package com.ant.goldenticket;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ant.goldenticket.dao.DAOLocalita;
 import com.ant.goldenticket.dao.Database;
 
 @Configuration
@@ -10,5 +11,10 @@ public class Context {
 	@Bean
 	public Database db() {
 		return new Database("GoldenTicket","root","root");
+	}
+	@Bean 
+	DAOLocalita daolocalita()
+	{
+		return new DAOLocalita();
 	}
 }
