9e98cf49b233060f5a6743b2a9833ad81199c153
Andrea Sbabo <andreasbabo6@gmail.com>
2023-06-27 10:10:31 +0200

    done

--- goldenTicket/src/main/java/com/ant/goldenticket/controllers/AdminController.java
+++ goldenTicket/src/main/java/com/ant/goldenticket/controllers/AdminController.java
@@ -1,13 +1,10 @@
 package com.ant.goldenticket.controllers;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
